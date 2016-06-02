b9e0839ca6940060ae8d3aab6d4662a3a0f06bb2
Yue Chen <e5z8@bowen.ugrad.cs.ubc.ca>
2016-06-02 15:26:31 -0700

    Commit

--- lib/config.php
+++ lib/config.php
@@ -1,10 +1,10 @@
 <?php
 
 /* Database User Name */
-define('DB_USER', '');
+define('DB_USER', 'ora_e5z8');
 
 /* Database Password */
-define('DB_PASS', '');
+define('DB_PASS', 'a90294133');
 
 /* Database Name */
 define('DB_DATABASENAME', 'dbhost.ugrad.cs.ubc.ca:1522/ug');
