7b9eedd2583401e34c8a93a106d60f54d5ca7912
Yue Chen <mark_c1995@hotmail.com>
2016-06-11 16:19:00 -0700

    Blah

--- lib/config.php
+++ lib/config.php
@@ -1,10 +1,10 @@
 <?php
 
 /* Database User Name */
-define('DB_USER', 'ora_t1k0b');
+define('DB_USER', 'ora_e5z8');
 
 /* Database Password */
-define('DB_PASS', 'a12034147');
+define('DB_PASS', 'a90294133');
 
 /* Database Name */
 define('DB_DATABASENAME', 'dbhost.ugrad.cs.ubc.ca:1522/ug');
